cda49f091bf19ef06982eeb6ddd3ece12a03b954
PabloSato <pablofersato@gmail.com>
2023-10-12 17:30:57 +0000

    custom templates and nav_links

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,27 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+           Coders Free
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            {{--  ASÍ SE LLAMA A UN COMPONENTE DE JETSTREAM-LIVEWIRE  --}}
-            <x-welcome></x-welcome>
-
-            {{-- ASÏ SE LLAMAN A NUESTROS COMPONENTES
-                @php
-                $type = 'info';
-                @endphp
-                <x-alert2 :type="$type">
-                    <x-slot name="title">
-                        Mira que te lo digo!!
-                    </x-slot>
-                    <p>EL FIN DEL MUNDO SE APROXIMA!!!</p>
-                </x-alert2>
-                --}}
+           <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel modi reiciendis cumque perferendis nobis quisquam velit ipsum! A deserunt itaque, esse amet, repellendus assumenda unde consectetur omnis magni, ad illo.</p>
+            <p>Harum hic cumque nobis, nulla, vitae saepe est placeat, ab officiis doloremque sapiente? Iusto, consequatur exercitationem praesentium unde aliquam ab ipsa error eius eveniet tenetur nulla laborum officia, at sunt.</p>
+            <p>Quasi exercitationem corporis, alias optio, cupiditate necessitatibus magnam voluptatum laborum facilis placeat unde totam velit sit quas modi eveniet earum reprehenderit, quia provident asperiores ducimus? Magni unde ipsam reprehenderit eius.</p>
+            <p>Temporibus ullam at, deserunt voluptatem beatae itaque incidunt quibusdam inventore quam vel. Quisquam quam aspernatur soluta quaerat illo doloribus rem sint, iste tempore consequatur minima, libero molestias, tenetur optio dicta.</p>
+            <p>Natus, incidunt praesentium. Quas enim officiis ab labore! Architecto deleniti nam at reiciendis earum accusantium? Architecto quaerat, ex dicta amet, molestiae porro veritatis placeat aperiam vel vero eaque totam et.</p>
+           </div>
 
         </div>
     </div>
