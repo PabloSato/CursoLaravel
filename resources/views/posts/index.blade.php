9ccc94539842b6f87202bb36063e6440090ecfc4
PabloSato <pablofersato@gmail.com>
2023-10-15 17:49:06 +0000

    fin front ya vamos con admilte...

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,11 +8,11 @@
                     <div class="w-full h-full px-8 flex flex-col justify-center">
                         <div>
                             @foreach ($post->tags as $tag)
-                                <a href="#"
+                                <a href="{{ route('posts.tag', $tag) }}"
                                     class="inline-block px-2 h-6 bg-{{ $tag->color }}-600 text-white rounded-full">{{ $tag->name }}</a>
                             @endforeach
                         </div>
-                        <h1 class="text-4xl text-white leading-8 font-bold"><a
+                        <h1 class="text-4xl text-white leading-8 font-bold mt-2"><a
                                 href="{{ route('posts.show', $post) }}">{{ $post->name }}</a></h1>
                     </div>
                 </article>
